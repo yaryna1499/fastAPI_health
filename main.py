a4f0b659a57872870198aa18dbd6d63ba389adac
yaryna1499 <yyyzzz9914@gmail.com>
2024-08-27 16:05:02 +0300

    _

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-"""Entrypoint to invoke the FastAPI application service with."""
-
-from fastapi import FastAPI, status
-from pydantic import BaseModel
+import logging
+import random
+import time
+from typing import Optional
 import uvicorn
+from fastapi import FastAPI, Response
+from metrics import PrometheusMiddleware, metrics, setting_otlp
 from multiprocessing import Queue
 from logging_loki import LokiQueueHandler
-import logging
-from starlette_exporter import PrometheusMiddleware, handle_metrics
+from pydantic import BaseModel
+from starlette import status 
 
+APP_NAME = "app_test"
+EXPOSE_PORT = 8000
+OTLP_GRPC_ENDPOINT = "http://18.235.248.167:4317"
 
 app = FastAPI()
-app.add_middleware(PrometheusMiddleware, app_name="hello_world", skip_paths=["/metrics"])
-app.add_route("/metrics", handle_metrics)
-
-
 loki_logs_handler = LokiQueueHandler(
     Queue(-1),
     url="http://18.235.248.167:3100/loki/api/v1/push",
@@ -23,6 +24,22 @@ loki_logs_handler = LokiQueueHandler(
 
 uvicorn_access_logger = logging.getLogger("uvicorn.access")
 uvicorn_access_logger.addHandler(loki_logs_handler)
+# Setting metrics middleware
+app.add_middleware(PrometheusMiddleware, app_name=APP_NAME)
+app.add_route("/metrics", metrics)
+
+# Setting OpenTelemetry exporter
+setting_otlp(app, APP_NAME, OTLP_GRPC_ENDPOINT)
+
+
+class EndpointFilter(logging.Filter):
+    # Uvicorn endpoint access log filter
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find("GET /metrics") == -1
+
+
+# Filter out /endpoint
+uvicorn_access_logger.addFilter(EndpointFilter())
 
 
 class HealthCheck(BaseModel):
@@ -49,27 +66,63 @@ def get_health() -> HealthCheck:
     Returns:
         HealthCheck: Returns a JSON response with the health status
     """
-    logging.error("test error")
+    uvicorn_access_logger.error("test error")
+    uvicorn_access_logger.exception("test exception")
+    uvicorn_access_logger.warning("test warning")
     return HealthCheck(status="OK")
 
 
-def main() -> None:
-    """Entrypoint to invoke when this module is invoked on the remote server."""
-    # See the official documentations on how "0.0.0.0" makes the service available on
-    # the local network - https://www.uvicorn.org/settings/#socket-binding
-    uvicorn.run("main:app", host="0.0.0.0")
+@app.get("/")
+async def read_root():
+    logging.error("Hello World")
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    logging.error("items")
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/io_task")
+async def io_task():
+    time.sleep(1)
+    logging.error("io task")
+    return "IO bound task finish!"
+
+
+@app.get("/cpu_task")
+async def cpu_task():
+    for i in range(1000):
+        _ = i * i * i
+    logging.error("cpu task")
+    return "CPU bound task finish!"
+
+
+@app.get("/random_status")
+async def random_status(response: Response):
+    response.status_code = random.choice([200, 200, 300, 400, 500])
+    logging.error("random status")
+    return {"path": "/random_status"}
+
+
+@app.get("/random_sleep")
+async def random_sleep(response: Response):
+    time.sleep(random.randint(0, 5))
+    logging.error("random sleep")
+    return {"path": "/random_sleep"}
+
+
+@app.get("/error_test")
+async def error_test(response: Response):
+    logging.error("got error!!!!")
+    raise ValueError("value error")
 
 
 if __name__ == "__main__":
-    main()
-
-"""
-To check if the API service works as expected, perform the following actions:
-    1. Run the API service by invoking this command - "python -m main".
-    2. If the service is running, open the URL "http://localhost:8000" in your browser.
-    3. With cURL, invoke this command:
-       "curl --include --request GET "http://localhost:8000/health" and you should
-       get a HTTP Status Code 200 OK message somewhere in it."
-An example Dockerfile with a healthcheck capabilities enabled is available in this gist:
-https://gist.github.com/Jarmos-san/11bf22c59d26daf0aa5223bdd50440da
-"""
+    # update uvicorn access logger format
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"][
+        "fmt"
+    ] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
+    uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
