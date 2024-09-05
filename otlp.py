b4650661dba9353b68c7147f7fdf678e95ec68e0
yaryna1499 <yyyzzz9914@gmail.com>
2024-09-05 16:51:28 +0300

    tst traces

--- otlp.py
+++ otlp.py
@@ -22,9 +22,7 @@ def set_otlp(app: ASGIApp, app_name: str, otlp_endp: str):
     tracer = TracerProvider(resource=resource)
     trace.set_tracer_provider(tracer)
     tracer.add_span_processor(BatchSpanProcessor(OTLPSpanExporter(endpoint=otlp_endp)))
-    # collect traces from logging events
-    LoggingInstrumentor().instrument(set_logging_format=True)
-    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
+    
     # LOGGING
     logger_provider = LoggerProvider(resource=resource)
     set_logger_provider(logger_provider)
@@ -34,6 +32,10 @@ def set_otlp(app: ASGIApp, app_name: str, otlp_endp: str):
     uvicorn_logger.addHandler(handler)
     formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s")
     handler.setFormatter(formatter)
+
+    # collect traces from logging events
+    LoggingInstrumentor().instrument(set_logging_format=True)
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
 
 
 
