253c69fb5af1e3b333ebc63b35317b04cf1dd5e7
yaryna1499 <yyyzzz9914@gmail.com>
2024-09-16 16:33:23 +0300

    logging testing

--- logger.py
+++ logger.py
@@ -17,28 +17,19 @@ logger.addFilter(EndpointFilter())
 import json
 
 class JSONFormatter(logging.Formatter):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self._ignore_keys = {"msg", "args"}
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = record.__dict__.copy()
-        message["message"] = record.getMessage()
-
-        for key in self._ignore_keys:
-            message.pop(key, None)
-
-        if record.exc_info and record.exc_text is None:
-            record.exc_text = self.formatException(record.exc_info)
-
-        if record.exc_text:
-            message["exc_info"] = record.exc_text
-
-        if record.stack_info:
-            message["stack_info"] = self.formatStack(record.stack_info)
-
-        return json.dumps(message)
+    def format(self, record):
+        log_record = {
+            'time': self.formatTime(record, self.datefmt),
+            'level': record.levelname,
+            'name': record.name,
+            'filename': record.filename,
+            'line': record.lineno,
+            'trace_id': getattr(record, 'otelTraceID', None),
+            'span_id': getattr(record, 'otelSpanID', None),
+            'service_name': getattr(record, 'otelServiceName', None),
+            'message': record.getMessage(),
+        }
+        return json.dumps(log_record)
 
 
 # stream_handler.setFormatter(JSONFormatter())
